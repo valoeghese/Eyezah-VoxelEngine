7b276e05efd8162e1c3dc6772b44d0576bd668b0
valoeghese <valoeghesemods@gmail.com>
2021-06-10 23:54:23 +1300

    more functionality

--- eyezah/engine.py
+++ eyezah/engine.py
@@ -9,16 +9,44 @@ WEST = "west"
 UP = "up"
 DOWN = "down"
 
+class Voxels:
+    AIR = 0
+    DATA = 1
+    RED = 2
+    BLUE = 3
+    ENGINE = 4
+
 class Engine:
     def __init__(self, name):
-        self.engineloc = "?engine=" + name + "&"
+        self.engineloc = "?engine=" + name
 
     def send(self, typ, data):
-        return request.urlopen(_base_url + typ + self.engineloc + parse.urlencode(data))
+        print(_base_url + typ + self.engineloc + (parse.urlencode(data) if data is not None else ""))
+        return request.urlopen(_base_url + typ + self.engineloc + (("&" + parse.urlencode(data)) if data is not None else ""))
 
+    """
+    Move the engine in the direction provided
+    """
     def move(self, direction):
         return self.send("engine-move", {'direction': direction})
-    
+
+    """
+    Sets a voxel with the specified type in the direction provided
+    """
+    def set(self, direction, typ):
+        return self.send("voxel-set", {'direction': direction, 'type': typ})
+
+    """
+    Sets a data voxel with the specified data string in the direction provided
+    """
+    def set_data(self, direction, data):
+        return self.send("voxel-set", {'direction': direction, 'type': Voxels.DATA, 'data': data})
+
+    """
+    Retrieve Data in a Status Around the Engine
+    """
+    def get_status(self):
+        return self.send("engine-status", None)
 
 
 
--- script.py
+++ script.py
@@ -1,5 +1,16 @@
 from eyezah.engine import *
+import time
 
 mekal = Engine("mekal-engine")
+
 print(mekal.move(UP).read())
+time.sleep(1)
+print(mekal.set(DOWN, Voxels.RED).read())
+time.sleep(1)
+print(mekal.move(UP).read())
+time.sleep(1)
+print(mekal.set_data(DOWN, "hello, world!").read())
+time.sleep(1)
+
+print(mekal.get_status().read())
 
